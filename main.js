d012f6aa75dcc1ab2fe53e5110424f2676c1a35e
Zcool99 <zachary.ehle@gmail.com>
2018-05-22 07:39:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-//Getting atoms
+//Initalising variables
 
 var Atoms = 9999999;
 var Elements = 0;
 var Molecules = 0;
 var Compunds = 0;
+var Aloys =  0;
+var matter = 0;
 
+// clicking to get atoms
 var AutoClick = Elements + (Molecules * 2) + (Compunds * 3);
 document.getElementById('AutoClick').innerHTML = AutoClick;
 
@@ -13,6 +16,8 @@ function AtomClick(number) {
   document.getElementById("Atoms").innerHTML = Atoms;
 };
 
+//upgrades
+//Elements
 function buyElement() {
   var ElementCost = Math.floor(10 * Math.pow(1.3, Elements)); //works out the cost of this Element
   if (Atoms >= ElementCost) { //checks that the player can afford the Element
@@ -25,7 +30,7 @@ function buyElement() {
   document.getElementById('ElementCost').innerHTML = nextECost; //updates the Element cost for the user
 };
 
-
+//Molecules
 function buyMolecule() {
   var MoleculeCost = Math.floor(100 * Math.pow(1.5, Molecules)); //works out the cost of this Molecule
   if (Atoms >= MoleculeCost) { //checks that the player can afford the Molecule
@@ -38,7 +43,7 @@ function buyMolecule() {
   document.getElementById('MoleculeCost').innerHTML = nextMCost; //updates the Molecule cost for the user
 };
 
-
+//Compunds
 function buyCompund() {
   var CompundCost = Math.floor(1000 * Math.pow(1.75, Compunds)); //works out the cost of this Compund
   if (Atoms >= CompundCost) { //checks that the player can afford the Compund
@@ -51,7 +56,34 @@ function buyCompund() {
   document.getElementById('CompundCost').innerHTML = nextCCost; //updates the Compund cost for the user
 };
 
+//Aloys
+function buyAloy() {
+  var AloyCost = Math.floor(10000 * Math.pow(1.85, Aloys)); //works out the cost of this Aloy
+  if (Atoms >= AloyCost) { //checks that the player can afford the Aloy
+    Aloys++; //increases number of Aloys
+    Atoms -= AloyCost; //removes the Atoms spent
+    document.getElementById('Aloys').innerHTML = Aloys; //updates the number of Aloys for the user
+    document.getElementById('Atoms').innerHTML = Atoms; //updates the number of Atoms for the user
+  };
+  var nextACost = Math.floor(1000 * Math.pow(1.85, Aloys)); //works out the cost of the next Aloy
+  document.getElementById('AloyCost').innerHTML = nextACost; //updates the Aloy cost for the user
+};
 
+//Matter
+function buyMatter() {
+  var MatterCost = Math.floor(10000 * Math.pow(1.85, Aloys)); //works out the cost of the Matter
+  if (Atoms >= MatterCost) { //checks that the player can afford the Matter
+    Matter++; //increases amount of Matter
+    Atoms -= MatterCost; //removes the Atoms spent
+    document.getElementById('Matter').innerHTML = Matter; //updates the amount of Matter for the user
+    document.getElementById('Atoms').innerHTML = Atoms; //updates the number of Atoms for the user
+  };
+  var nextMaCost = Math.floor(1000 * Math.pow(1.9, Matter)); //works out the cost of the next Matter
+  document.getElementById('MatterCost').innerHTML = nextMaCost; //updates the Matter cost for the user
+};
+
+//Next Upgrade Dark Matter (DarkMatter or DMatter)
+//Auto click
 window.setInterval(function() {
 
   AtomClick(Elements + (Molecules * 2)+(Compunds * 3));
